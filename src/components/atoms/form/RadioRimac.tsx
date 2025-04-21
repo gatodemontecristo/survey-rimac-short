65097200d1d0a67d59a92c303597336ec9071d07
gatodemontecristo <erickdevicente2024@outlook.com>
2025-04-20 22:57:59 -0500

    feat: Resolved bugs

--- src/components/atoms/form/RadioRimac.tsx
+++ src/components/atoms/form/RadioRimac.tsx
@@ -10,11 +10,13 @@ import '../../../styles/radio-style.css';
 
 export const RadioRimac = React.forwardRef<HTMLInputElement, RadioRimacProps>(
   ({ value, label, checked, onChange }, ref) => {
+    const id = `${label}-${value}`;
     return (
       <div className='flex items-center '>
-        <label className='flex items-center cursor-pointer'>
+        <label htmlFor={id} className='flex items-center cursor-pointer'>
           <input
             type='radio'
+            id={id}
             ref={ref}
             value={value}
             checked={checked}
